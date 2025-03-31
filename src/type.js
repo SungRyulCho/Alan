635219fe589d03e76e5e2accc18d93a251dc7d67
Sung Ryul Cho <hahahoho5811@gmail.com>
2025-04-01 00:10:14 +0900

    Typeit-Change

--- src/type.js
+++ src/type.js
@@ -1,3 +1,23 @@
 "use strict";
 
-new TypeIt(".home__title__strong").go();
+new TypeIt(".home__title__strong", {
+  loop: true,
+  speed: 100,
+}) // Dream Coder|
+  .move(-11)
+  .type("Amazing ")
+  .pause(1000)
+  .move(null, { to: "END" })
+  .delete() //|
+  .type("Front-end Engineer")
+  .pause(1000)
+  .move(-9) //
+  .delete(9) //
+  .type("Back-end")
+  .pause(1000)
+  .delete(8)
+  .type("Full-stack")
+  .pause(1000)
+  .move(9)
+  .delete()
+  .go();
